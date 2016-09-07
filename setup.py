034264868f5a624664305beaf11644c34d88f455
Sasha Vladimirskiy <sasha@butchershop.co>
2016-09-07 10:35:44 -0700

    Lint setup module

--- setup.py
+++ setup.py
@@ -1,43 +1,39 @@
 from setuptools import setup, find_packages
-import os
 
-version = '0.1'
+version = '0.2'
 
 long_description = (
-    open('README.txt').read()
-    + '\n' +
+    open('README.txt').read() + '\n' +
     'Contributors\n'
-    '============\n'
-    + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
-    + '\n')
+    '============\n' + '\n' +
+    open('CONTRIBUTORS.txt').read() + '\n' +
+    open('CHANGES.txt').read() + '\n')
 
-setup(name='Mezzanine Facebook',
-      version=version,
-      description="Facebook app for Django Mezzanine CMS",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='Mezzanine Facebook',
+    version=version,
+    description="Facebook app for Django Mezzanine CMS",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Aleksandr Vladimirskiy',
-      author_email='aleksandr@butchershopcreative.com',
-      url='http://www.butchershopcreative.com/',
-      license='bsd',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['mezzanine'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    keywords='',
+    author='Aleksandr Vladimirskiy',
+    author_email='sasha@butchershop.co',
+    url='http://www.butchershop.co/',
+    license='bsd',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['mezzanine'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
